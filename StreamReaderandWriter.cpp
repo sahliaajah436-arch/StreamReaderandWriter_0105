a2ff576fafd4e5eb20e4d9eefc56c44b10d3e25c
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-06-02 18:28:04 +0700

    menambahkan return

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -29,4 +29,28 @@ int main()
 
     }
 
- 
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada 
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+         //menutup file jika telah selesai
+        infile.close();
+    }
+
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file" ;
+    return 0;
+    
+}
