1459546a40f30260093373ef3e80c67483ac20cf
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-06-02 18:27:16 +0700

    menambahkan fungsi main

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -2,3 +2,31 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris untuk menyimpan data string 
+    string baris;
+    //membuka file dalam node menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+
+        cout << "-";
+        //mendapat setiap karakter dalam satu baris
+         getline(cin, baris);
+         //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+
+    }
+
+ 
